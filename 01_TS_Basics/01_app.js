1648b76aea0943bc1a998da5ca4cc7b50f113580
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-06-11 14:04:51 +0530

    Initial commit

---
+++ 01_TS_Basics/01_app.js
@@ -0,0 +1,15 @@
+// Hello World App
+var greeting = "Good Morning TS !!";
+console.log(greeting);
+document.getElementById('display').innerHTML = greeting;
+/*
+
+DataTypes in TypeScript
+
+1.Boolean
+2.Number
+3.String
+4.array[]
+5.any
+
+ */
---
+++ 01_TS_Basics/01_app.ts
@@ -0,0 +1,18 @@
+
+
+// Hello World App
+let greeting:string ="Good Morning TS !!";
+console.log(greeting);
+document.getElementById('display').innerHTML = greeting;
+/*
+
+DataTypes in TypeScript
+
+1.Boolean
+2.Number
+3.String
+4.array[]
+5.any
+
+ */
+
